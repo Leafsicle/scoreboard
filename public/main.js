75d0cbdca671326662318a245f95d642c187e8d3
Matt Cooke <Mattcookemed@gmail.com>
2018-09-12 08:58:21 -0400

    this is a hot mess

--- public/main.js
+++ public/main.js
@@ -1,27 +1,14 @@
-const main = () => {
-  let clickNum = 0;
-
-  const clicky = event => {
-    clickNum += 1;
-  };
-  let message = `hey, we clicked ${clickNum} times`;
-  console.log(event.target);
-  event.target.textcontent = message;
-
-  let matchingElements = document.querySelectorAll(".add");
-
-  let lastElement = matchingElements[1];
-
-  lastElement.textContent += "?";
-
-  let button = document.querySelector("button.first");
-
-  button.addEventListener("click", clicky);
-
-  let button2 = document.querySelector("button.second");
-  button2.addEventListener("click", clicky);
-
-  let button3 = document.querySelector("button.third");
-  button3.addEventListener("click", clicky);
+let score = 0;
+const add1team1 = event => {
+  score += 1;
+  let display = `${score}`;
+  const team1score = document.querySelector(".team1 .score");
+  team1score.textContent = display;
 };
+
+const main = () => {
+  const addbuttonteam1 = document.querySelector(".team1 .add");
+  addbuttonteam1.addEventListener("click", add1team1);
+};
+
 document.addEventListener("DOMContentLoaded", main);
