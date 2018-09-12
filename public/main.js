9e085b04361f5fbd8fb7bc7dcf0a38c9d5002d91
Matt Cooke <Mattcookemed@gmail.com>
2018-09-11 22:57:04 -0400

    css html revised tags

--- public/main.js
+++ public/main.js
@@ -1,5 +1,27 @@
 const main = () => {
-  document.querySelector("h1").textContent += "";
-};
+  let clickNum = 0;
 
+  const clicky = event => {
+    clickNum += 1;
+  };
+  let message = `hey, we clicked ${clickNum} times`;
+  console.log(event.target);
+  event.target.textcontent = message;
+
+  let matchingElements = document.querySelectorAll(".add");
+
+  let lastElement = matchingElements[1];
+
+  lastElement.textContent += "?";
+
+  let button = document.querySelector("button.first");
+
+  button.addEventListener("click", clicky);
+
+  let button2 = document.querySelector("button.second");
+  button2.addEventListener("click", clicky);
+
+  let button3 = document.querySelector("button.third");
+  button3.addEventListener("click", clicky);
+};
 document.addEventListener("DOMContentLoaded", main);
